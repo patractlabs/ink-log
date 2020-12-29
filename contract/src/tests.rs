a038b368c8074fe250a7bc512f58234202cf50e7
tylerztl <tailinzhang1993@gmail.com>
2020-12-29 16:36:48 +0800

    ignore print error (#1)
    * ignore print error
    
    * update readme
    
    * optimise dependent libraries

--- contract/src/tests.rs
+++ contract/src/tests.rs
@@ -2,6 +2,7 @@
 mod tests {
     #[test]
     fn test_ink_log() {
+        // TODO
         // ink_env::test::register_chain_extension::<ink_env::DefaultEnvironment, log::LogRecord, log::LogRecord>()
 
         crate::off_chain::log(log::Level::Info as u32, "test", "pretty ink log");
