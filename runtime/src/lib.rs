ad32b152c342da59f9ead06925bccfffb1a74ef5
zqh <zqhxuyuan@gmail.com>
2021-07-15 19:29:15 +0800

    read as

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -52,7 +52,8 @@ macro_rules! logger_ext {
                     DispatchError::Other("LogRecord parse failed")
                 }
                 // The memory of the vm stores buf in scale-codec
-                let input: LoggerExt = env.read_as()?;
+                let len = env.in_len();
+                let input: LoggerExt = env.read_as_unbounded(len)?;               
                 let target = str::from_utf8(input.target.as_slice()).map_err(dispatch_error)?;
                 let args = str::from_utf8(input.args.as_slice()).map_err(dispatch_error)?;
 
